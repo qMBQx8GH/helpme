267e679863d76b38cacf1c4bb2f989239bbc814c
Maxim Smirnov <qmbqx8gh@gmailcom>
2019-12-11 14:52:38 +0200

    Wiki.ru links

--- src/Wiki.ru/makehtml.py
+++ src/Wiki.ru/makehtml.py
@@ -1,5 +1,4 @@
 # -*- coding: UTF-8 -*-
-
 import json
 import urllib
 
@@ -11,13 +10,20 @@ newlist = sorted(links, key=lambda k: k['id_str'])
 
 f = open('out\\test.html', 'wb')
 f.write("<html><body><table>\n")
-f.write("<tr><th>id_str</th><th>nation</th><th>type</th><th>tier</th><th>ship_id</th><th>ship_name</th><th>url</th></tr>\n")
+f.write("<tr><th>id_str</th><th>nation</th><th>type</th><th>tier</th><th>ship_id</th><th>ship_name</th><th>url</th><th>status</th></tr>\n")
 for ship in newlist:
-    f.write('<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td nowrap>' % (ship['id_str'], ship['nation'], ship['type'], ship['tier'], ship['ship_id']))
-    f.write(ship['ship_name'].encode('utf8'))
-    f.write('</td><td nowrap><a href="%s">' % ship['url'])
-    url = urllib.unquote(ship['url'].encode('utf8'))
-    f.write(url)
-    f.write("</a></td></tr>\n")
+    f.write('<tr {}><td>{}</td><td>{}</td><td>{}</td><td>{}</td><td>{}</td><td nowrap>{}</td><td nowrap><a href="{}">{}</a></td><td>{}</td></tr>'.format(
+        'style="background-color:yellow"' if ship['status_code'] != 200 else '',
+        ship['id_str'],
+        ship['nation'],
+        ship['type'],
+        ship['tier'],
+        ship['ship_id'],
+        ship['ship_name'].encode('utf8'),
+        ship['url'],
+        urllib.unquote(ship['url'].encode('utf8')),
+        ship['status_code']
+    ))
+    f.write("\n")
 f.write("</table></body></html>\n")
 f.close()
